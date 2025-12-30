8de66ecb647ad74f1f3c12841feae98dc07f6da6
voidalcon <arturgs.oliveira@pm.me>
2025-12-30 19:08:53 -0300

    feat(api): implementar endpoint de saudação na rota raiz - Adiciona um endpoint GET na rota raiz (/) que retorna 'Hello, World!' - Habilita a classe BackendApplication como um RestController - Inclui o arquivo .tool-versions para gerenciar a versão do Java (temurin-21.0.9+10.0.LTS) - Adiciona .env ao .gitignore para exclusão de variáveis de ambiente

--- backend/src/main/java/br/com/oriontask/backend/BackendApplication.java
+++ backend/src/main/java/br/com/oriontask/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package br.com.oriontask.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@GetMapping()
+	public String hello() {
+		return "Hello, World!";
 	}
 
 }
