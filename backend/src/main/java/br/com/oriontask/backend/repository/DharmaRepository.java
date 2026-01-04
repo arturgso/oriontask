0d3483baccec930619c96000284f5758a124b8b1
vexis <arturgs.dev@gmail.com>
2026-01-03 22:01:38 -0300

    feat: add findByUserIdAndHiddenFalse query to DharmaRepository

--- backend/src/main/java/br/com/oriontask/backend/repository/DharmaRepository.java
+++ backend/src/main/java/br/com/oriontask/backend/repository/DharmaRepository.java
@@ -13,4 +13,5 @@ public interface DharmaRepository extends JpaRepository<Dharma, Long>{
     Optional<Dharma> findById(Long id);
     Long countByUser(Users user);
     List<Dharma> findByUserId(UUID userId);
+    List<Dharma> findByUserIdAndHiddenFalse(UUID userId);
 }
